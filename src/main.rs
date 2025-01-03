c4b61c332b858e66f2e3ca75a2ed2c00d4eb4116
boss <bossxlp@gmail.com>
2025-01-03 08:32:22 +0700

    feat: add migration to pg db and create funtion to connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use quests_tracker::config::config_loader;
+use quests_tracker::{config::config_loader, infrastructure::postgres::postgres_connection};
 use tracing::{error, info};
 
 #[tokio::main]
@@ -16,4 +16,14 @@ async fn main(){
     };
 
     info!("ENV has been loaded");
+
+    let postgres_pool = match postgres_connection::establish_connection(&dotenvy_env.database.url) {
+        Ok(pool) => pool,
+        Err(e) => {
+            error!("Failed to establish connection to Postgres: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    info!("Postgres connection has been established");
 }
